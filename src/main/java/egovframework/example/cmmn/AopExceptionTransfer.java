f05366eb6175532d7a69070bec51d9a5109d999d
wanbaejeong <wanbae.jeong@bankwareglobal.com>
2023-06-16 13:49:25 +0900

    주석 추가했습니다!!!
    @author, @since, @version
    
    위에 3가지느 정확히 몰라서 다른곳에 있는것과 동일한 내용으로 입력했습니다.

--- src/main/java/egovframework/example/cmmn/AopExceptionTransfer.java
+++ src/main/java/egovframework/example/cmmn/AopExceptionTransfer.java
@@ -7,6 +7,22 @@ import org.aspectj.lang.annotation.Pointcut;
 
 import org.egovframe.rte.fdl.cmmn.aspect.ExceptionTransfer;
 
+/**
+ * @Class Name : AopExceptionTransfer.java
+ * @Description : AopExceptionTransfer Class
+ * @Modification Information
+ * @
+ * @  수정일      수정자              수정내용
+ * @ ---------   ---------   -------------------------------
+ * @ 2023.06.16           최초생성
+ *
+ * @author 개발프레임웍크 실행환경 개발팀
+ * @since 2009. 03.16
+ * @version 1.0
+ * @see
+ *
+ *  Copyright (C) by MOPAS All right reserved.
+ */
 @Aspect
 public class AopExceptionTransfer {
 
