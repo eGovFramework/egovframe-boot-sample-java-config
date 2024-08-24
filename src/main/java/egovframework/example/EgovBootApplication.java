5a44aa179487efba86dd40d3b06f6192c92f9671
이백행 <dlqorgod@naver.com>
2024-08-24 11:02:59 +0900

    UseUtilityClass 시큐어코딩 PMD 진단/조치

--- src/main/java/egovframework/example/EgovBootApplication.java
+++ src/main/java/egovframework/example/EgovBootApplication.java
@@ -1,13 +1,32 @@
 package egovframework.example;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EgovBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgovBootApplication.class, args);
+	}
+
+	/**
+	 * 애플리케이션 러너
+	 * 
+	 * @return
+	 */
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return args -> {
+			if (log.isDebugEnabled()) {
+				log.debug("args={}", args);
+			}
+		};
 	}
 
 }
